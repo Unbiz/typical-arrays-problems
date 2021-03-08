62cc1a8e14bb9bb5ca56f1a25465dde0426f81ed
Vladimir Bessonov <bessmy@gmail.com>
2021-03-08 11:59:38 +0300

    feat: add functions to find max, min and avg of array items

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = null) {
+  if (array === null || array.length === 0) return 0;
+  return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = null) {
+  if (array === null || array.length === 0) return 0;
+  return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = null) {
+  if (array === null || array.length === 0) return 0;
+  const sum = array.reduce((acc, item) => acc + item);
+  const avg = sum / array.length;
+  return avg.toFixed(2);
 }
